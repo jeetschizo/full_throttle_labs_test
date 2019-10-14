20f7887e890675b0cb7ec96f42ca9c90daa3ba45
Jeet Padhya <jeet.padhya@hotmail.com>
2019-10-15 03:31:06 +0530

    Increasing text max length to 50

--- words/models.py
+++ words/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Word(models.Model):
 
-    text = models.CharField(max_length=30)
+    text = models.CharField(max_length=50)
     occurence = models.BigIntegerField()
 
     class Meta:
