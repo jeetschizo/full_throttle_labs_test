e8d76dc65e559bd69b09ad9d0e16597edaac8c46
Jeet Padhya <jeet.padhya@hotmail.com>
2019-10-15 03:09:59 +0530

    Testing another config for heroku

--- full_throttle_labs/settings.py
+++ full_throttle_labs/settings.py
@@ -122,5 +122,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-# prod_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
