44631bc5a31a8575d20bd31b6150b8d307abadd7
Jeet Padhya <jeet.padhya@hotmail.com>
2019-10-14 23:17:21 +0530

    Changes to settings.py - moving import to top of file

--- full_throttle_labs/settings.py
+++ full_throttle_labs/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 """
 Django settings for full_throttle_labs project.
 
@@ -122,6 +123,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
